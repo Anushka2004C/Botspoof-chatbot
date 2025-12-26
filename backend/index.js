d27f7eb2a396abae85960f9adec62c00a52cb768
Anushka@2004Chaturkar <anushkachaturkar8@gmail.com>
2025-12-26 20:54:26 +0530

    Botspoof

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import chatbotRoutes from './routes/chatbot.router.js';
+
+const app = express()
+dotenv.config()
+
+const port =process.env.PORT || 3000
+
+// middleware
+app.use(express.json());
+app.use(cors())
+
+app.post("/test", (req, res) => {
+  console.log("TEST BODY:", req.body);
+  res.json(req.body);
+});
+
+
+//Database Connection code
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("Connected to MongoDB")
+}).catch((error)=>{
+    console.log("Error connecting to MongoDB:", error)
+})
+
+// Defining Routes
+app.use("/bot/v1/", chatbotRoutes)
+
+app.listen(port, () => {
+  console.log(`Server is Running on Port ${port}`)
+})
